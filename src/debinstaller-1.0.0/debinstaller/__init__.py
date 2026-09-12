10d137df38317da900a10f96c02a67237926d44b
SpiritHerb <spiritherb@users.noreply.github.com>
2026-09-13 03:07:11 +0800

    chore: 排除构建产物（src/ pkg/ *.pkg.tar.zst *.tar.gz）

--- src/debinstaller-1.0.0/debinstaller/__init__.py
+++
@@ -1,7 +0,0 @@
-"""debinstaller - install Debian (.deb) packages on Arch Linux.
-
-Converts a .deb into a native pacman package and lets pacman manage it.
-"""
-
-__version__ = "0.1.0"
-__all__ = ["__version__"]
